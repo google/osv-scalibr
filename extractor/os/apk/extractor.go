d80f017d80c13e3c24146637f02e71c3d25f36bd
Yousef Alowayed <yalo@google.com>
2024-05-07 10:09:24 -0700

    Make apk and cos extractor names public constants.
    PiperOrigin-RevId: 631462835

--- extractor/os/apk/extractor.go
+++ extractor/os/apk/extractor.go
@@ -32,11 +32,16 @@ import (
 	"github.com/google/osv-scalibr/purl"
 )
 
+const (
+	// Name is the unique name of this extractor.
+	Name = "os/apk"
+)
+
 // Extractor extracts packages from the APK database.
 type Extractor struct{}
 
 // Name of the extractor.
-func (e Extractor) Name() string { return "os/apk" }
+func (e Extractor) Name() string { return Name }
 
 // Version of the extractor.
 func (e Extractor) Version() int { return 0 }
