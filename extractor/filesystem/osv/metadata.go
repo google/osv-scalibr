2b42bb52c9c1c8e36b92f10a0bc60440e5cee9d2
Rex P <rexpan@google.com>
2024-08-30 14:45:12 +1000

    Add python extractors

--- extractor/filesystem/osv/metadata.go
+++ extractor/filesystem/osv/metadata.go
@@ -21,3 +21,25 @@ type Metadata struct {
 	Ecosystem string
 	CompareAs string
 }
+
+type DepGroups interface {
+	DepGroups() []string
+}
+
+// osv.DepGroupMetadata is a metadata struct that only supports DepGroups
+type DepGroupMetadata struct {
+	DepGroupVals []string
+}
+
+var _ DepGroups = DepGroupMetadata{}
+
+func (dgm DepGroupMetadata) DepGroups() []string {
+	return dgm.DepGroupVals
+}
+
+// DistroVersionMetadata contains distro versions
+// This is not meant to be used directly. The distro version should be retrieved
+// from the Ecosystem() function.
+type DistroVersionMetadata struct {
+	DistroVersionStr string
+}
