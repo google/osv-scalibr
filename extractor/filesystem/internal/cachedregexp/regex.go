c71ca2aa25e22c1c0953eebd4fa4b8ad2b470120
Rex P <rexpan@google.com>
2024-09-02 16:36:54 +1000

    Add docstring for cachedregexp

--- extractor/filesystem/internal/cachedregexp/regex.go
+++ extractor/filesystem/internal/cachedregexp/regex.go
@@ -1,3 +1,4 @@
+// Package cachedregexp provides a cached MustCompile alternative to regexp
 package cachedregexp
 
 import (
@@ -7,6 +8,10 @@ import (
 
 var cache sync.Map
 
+// MustCompile returns the same Regexp that regexp.MustCompile returns.
+// The difference is MustCompile will also cache the compiled regexp
+// to a global cache, if the same input is called again, it will retrieve
+// the Regexp from the cache instead of recompiling.
 func MustCompile(exp string) *regexp.Regexp {
 	compiled, ok := cache.Load(exp)
 	if !ok {
