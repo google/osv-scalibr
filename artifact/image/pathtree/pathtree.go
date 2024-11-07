ffa162bf142e2718d010712367e5fa4edf80dc1d
SCALIBR Team <copybara-servicebot@google.com>
2024-11-07 08:35:59 -0800

    No public description
    PiperOrigin-RevId: 694129988

---
+++ artifact/image/pathtree/pathtree.go
@@ -0,0 +1,167 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package pathtree provides a tree structure for representing file paths.
+// Each path segment is a node in the tree, enabling efficient storage
+// and retrieval for building virtual file systems.
+package pathtree
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
+const divider string = "/"
+
+// ErrNodeAlreadyExists is returned when a node already exists at the given path. Given there
+// shouldn't be any duplicates to the tree, we should return this error if a node already exists.
+var ErrNodeAlreadyExists = errors.New("node already exists")
+
+// Node root represents the root directory /
+type Node[V any] struct {
+	value    *V
+	children map[string]*Node[V]
+}
+
+// NewNode creates a new node with the given value.
+func NewNode[V any]() *Node[V] {
+	return &Node[V]{
+		children: make(map[string]*Node[V]),
+	}
+}
+
+// Insert inserts a value into the tree at the given path.
+// If a node already exists at the given path, an error is returned.
+//
+// If a file is inserted without also inserting the parent directory
+// the parent directory entry will have a nil value.
+func (node *Node[V]) Insert(path string, value *V) error {
+	path, err := cleanPath(path)
+	if err != nil {
+		return fmt.Errorf("Insert() error: %w", err)
+	}
+
+	// If the path is empty, then this is the root node. The value should be set here before splitting
+	// the path.
+	if path == "" {
+		node.value = value
+		return nil
+	}
+
+	cursor := node
+	for _, segment := range strings.Split(path, divider) {
+		next, ok := cursor.children[segment]
+		// Create the segment if it doesn't exist
+		if !ok {
+			next = &Node[V]{
+				value:    nil,
+				children: make(map[string]*Node[V]),
+			}
+			cursor.children[segment] = next
+		}
+		cursor = next
+	}
+
+	if cursor.value != nil {
+		return fmt.Errorf("Insert(%q):%w", divider+path, ErrNodeAlreadyExists)
+	}
+
+	cursor.value = value
+
+	return nil
+}
+
+// getNode returns the node at the given path.
+func (node *Node[V]) getNode(path string) *Node[V] {
+	path, _ = cleanPath(path)
+
+	// If the path is empty, node is the root.
+	if path == "" {
+		return node
+	}
+
+	cursor := node
+	for _, segment := range strings.Split(path, divider) {
+		next, ok := cursor.children[segment]
+		if !ok {
+			return nil
+		}
+		cursor = next
+	}
+	return cursor
+}
+
+// Get retrieves the value at the given path.
+// If no node exists at the given path, nil is returned.
+func (node *Node[V]) Get(path string) *V {
+	pathNode := node.getNode(path)
+	if pathNode == nil {
+		return nil
+	}
+	return pathNode.value
+}
+
+// GetChildren retrieves all the direct children of the given path.
+func (node *Node[V]) GetChildren(path string) []*V {
+	pathNode := node.getNode(path)
+	if pathNode == nil {
+		return nil
+	}
+
+	var children = make([]*V, 0, len(pathNode.children))
+	for _, child := range pathNode.children {
+		// Some entries could be nil if a file is inserted without inserting the
+		// parent directories.
+		if child.value != nil {
+			children = append(children, child.value)
+		}
+	}
+
+	return children
+}
+
+// cleanPath returns a path for use in the tree. An error is returned if path is not formatted as
+// expected.
+func cleanPath(inputPath string) (string, error) {
+	path, found := strings.CutPrefix(inputPath, divider)
+	if !found {
+		return "", fmt.Errorf("path %q is not an absolute path", inputPath)
+	}
+
+	return path, nil
+}
+
+// Walk walks through all elements of this tree depths first, calling fn at every node
+func (node *Node[V]) Walk(fn func(string, *V) error) error {
+	return node.walk("", fn)
+}
+
+// walk is a recursive function for walking through the tree and calling fn at every node.
+func (node *Node[V]) walk(path string, fn func(string, *V) error) error {
+	// Only call fn if the node has a value.
+	if node.value != nil {
+		if err := fn(path, node.value); err != nil {
+			return err
+		}
+	}
+	for key, node := range node.children {
+		err := node.walk(path+divider+key, fn)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
---
+++ artifact/image/pathtree/pathtree_test.go
@@ -0,0 +1,286 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package pathtree
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+)
+
+type testVal struct {
+	string
+}
+
+func assertNoError(t *testing.T, err error) {
+	t.Helper()
+
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+}
+
+func testTree(t *testing.T) *Node[testVal] {
+	t.Helper()
+
+	tree := NewNode[testVal]()
+	assertNoError(t, tree.Insert("/", &testVal{"value0"}))
+	assertNoError(t, tree.Insert("/a", &testVal{"value1"}))
+	assertNoError(t, tree.Insert("/a/b", &testVal{"value2"}))
+	assertNoError(t, tree.Insert("/a/b/c", &testVal{"value3"}))
+	assertNoError(t, tree.Insert("/a/b/d", &testVal{"value4"}))
+	assertNoError(t, tree.Insert("/a/e", &testVal{"value5"}))
+	assertNoError(t, tree.Insert("/a/e/f", &testVal{"value6"}))
+	assertNoError(t, tree.Insert("/a/b/d/f", &testVal{"value7"}))
+	assertNoError(t, tree.Insert("/a/g", &testVal{"value8"}))
+	assertNoError(t, tree.Insert("/x/y/z", &testVal{"value9"}))
+
+	return tree
+}
+
+func TestNode_Insert_Error(t *testing.T) {
+	tests := []struct {
+		name string
+		tree *Node[testVal]
+		key  string
+		val  *testVal
+	}{
+		{
+			name: "duplicate node",
+			tree: func() *Node[testVal] {
+				tree := NewNode[testVal]()
+				_ = tree.Insert("/a", &testVal{"value1"})
+
+				return tree
+			}(),
+			key: "/a",
+			val: &testVal{"value2"},
+		},
+		{
+			name: "duplicate node in subtree",
+			tree: func() *Node[testVal] {
+				tree := NewNode[testVal]()
+				_ = tree.Insert("/a", &testVal{"value1"})
+				_ = tree.Insert("/a/b", &testVal{"value2"})
+
+				return tree
+			}(),
+			key: "/a/b",
+			val: &testVal{"value3"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.tree.Insert(tt.key, tt.val)
+			if err == nil {
+				t.Errorf("Node.Insert() expected error, got nil")
+			}
+		})
+	}
+}
+
+func TestNode_Get(t *testing.T) {
+	tests := []struct {
+		name string
+		tree *Node[testVal]
+		key  string
+		want *testVal
+	}{
+		{
+			name: "empty tree",
+			tree: NewNode[testVal](),
+			key:  "/a",
+			want: nil,
+		},
+		{
+			name: "single node",
+			tree: func() *Node[testVal] {
+				tree := NewNode[testVal]()
+				_ = tree.Insert("/a", &testVal{"value"})
+
+				return tree
+			}(),
+			key:  "/a",
+			want: &testVal{"value"},
+		},
+		{
+			name: "non-existent node in single node tree",
+			tree: func() *Node[testVal] {
+				tree := NewNode[testVal]()
+				_ = tree.Insert("/a", &testVal{"value"})
+
+				return tree
+			}(),
+			key:  "/b",
+			want: nil,
+		},
+		{
+			name: "root node",
+			tree: testTree(t),
+			key:  "/",
+			want: &testVal{"value0"},
+		},
+		{
+			name: "multiple nodes",
+			tree: testTree(t),
+			key:  "/a/b/c",
+			want: &testVal{"value3"},
+		},
+		{
+			name: "non-existent node",
+			tree: testTree(t),
+			key:  "/a/b/g",
+			want: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.tree.Get(tt.key)
+			if diff := cmp.Diff(tt.want, got, cmp.AllowUnexported(testVal{})); diff != "" {
+				t.Errorf("Node.Get() (-want +got): %v", diff)
+			}
+		})
+	}
+}
+
+func TestNode_GetChildren(t *testing.T) {
+	tests := []struct {
+		name string
+		tree *Node[testVal]
+		key  string
+		want []*testVal
+	}{
+		{
+			name: "empty tree",
+			tree: NewNode[testVal](),
+			key:  "/a",
+			want: nil,
+		},
+		{
+			name: "single node no children",
+			tree: func() *Node[testVal] {
+				tree := NewNode[testVal]()
+				_ = tree.Insert("/a", &testVal{"value"})
+
+				return tree
+			}(),
+			key:  "/a",
+			want: []*testVal{},
+		},
+		{
+			name: "root node",
+			tree: testTree(t),
+			key:  "/",
+			// /x is not included since the subdir is nil.
+			want: []*testVal{
+				{"value1"}, // "value1" is a value of /a
+			},
+		},
+		{
+			name: "multiple nodes with children",
+			tree: testTree(t),
+			key:  "/a/b",
+			want: []*testVal{
+				{"value3"},
+				{"value4"},
+			},
+		},
+		{
+			name: "non-existent node",
+			tree: testTree(t),
+			key:  "/a/b/g",
+			want: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.tree.GetChildren(tt.key)
+			if diff := cmp.Diff(tt.want, got, cmp.AllowUnexported(testVal{}), cmpopts.SortSlices(func(a, b *testVal) bool {
+				return strings.Compare(a.string, b.string) < 0
+			})); diff != "" {
+				t.Errorf("Node.GetChildren() (-want +got): %v", diff)
+			}
+		})
+	}
+}
+
+type keyValue struct {
+	key string
+	val string
+}
+
+func TestNode_Walk(t *testing.T) {
+	tests := []struct {
+		name string
+		tree *Node[testVal]
+		want []keyValue
+	}{
+		{
+			name: "empty tree",
+			tree: NewNode[testVal](),
+			want: []keyValue{},
+		},
+		{
+			name: "single node",
+			tree: func() *Node[testVal] {
+				tree := NewNode[testVal]()
+				_ = tree.Insert("/a", &testVal{"value"})
+
+				return tree
+			}(),
+			want: []keyValue{
+				{"/a", "value"},
+			},
+		},
+		{
+			name: "multiple nodes",
+			tree: testTree(t),
+			want: []keyValue{
+				{key: "", val: "value0"},
+				{key: "/a", val: "value1"},
+				{key: "/a/b", val: "value2"},
+				{key: "/a/b/c", val: "value3"},
+				{key: "/a/b/d", val: "value4"},
+				{key: "/a/e", val: "value5"},
+				{key: "/a/e/f", val: "value6"},
+				{key: "/a/b/d/f", val: "value7"},
+				{key: "/a/g", val: "value8"},
+				{key: "/x/y/z", val: "value9"},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := []keyValue{}
+			err := tt.tree.Walk(func(path string, node *testVal) error {
+				got = append(got, keyValue{key: path, val: node.string})
+				return nil
+			})
+			if err != nil {
+				t.Errorf("Node.Walk() error = %v", err)
+			}
+			if diff := cmp.Diff(tt.want, got, cmp.AllowUnexported(keyValue{}), cmpopts.SortSlices(func(a, b keyValue) bool {
+				if a.key == b.key {
+					return a.val < b.val
+				}
+				return a.key < b.key
+			})); diff != "" {
+				t.Errorf("Node.Walk() (-want +got): %v", diff)
+			}
+		})
+	}
+}
