28414ddb62f0ed0e597c6a0f16935a0d87bde505
Gareth Jones <jones258@gmail.com>
2024-12-02 09:22:17 +1300

    feat: add the `semantic` package

---
+++ semantic/parse.go
@@ -0,0 +1,58 @@
+package semantic
+
+import (
+	"errors"
+	"fmt"
+)
+
+var ErrUnsupportedEcosystem = errors.New("unsupported ecosystem")
+
+func MustParse(str string, ecosystem string) Version {
+	v, err := Parse(str, ecosystem)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return v
+}
+
+func Parse(str string, ecosystem string) (Version, error) {
+	//nolint:exhaustive // Using strings to specify ecosystem instead of lockfile types
+	switch ecosystem {
+	case "Alpine":
+		return parseAlpineVersion(str), nil
+	case "ConanCenter":
+		return parseSemverVersion(str), nil
+	case "CRAN":
+		return parseCRANVersion(str), nil
+	case "crates.io":
+		return parseSemverVersion(str), nil
+	case "Debian":
+		return parseDebianVersion(str), nil
+	case "Go":
+		return parseSemverVersion(str), nil
+	case "Hex":
+		return parseSemverVersion(str), nil
+	case "Maven":
+		return parseMavenVersion(str), nil
+	case "npm":
+		return parseSemverVersion(str), nil
+	case "NuGet":
+		return parseNuGetVersion(str), nil
+	case "Packagist":
+		return parsePackagistVersion(str), nil
+	case "Pub":
+		return parseSemverVersion(str), nil
+	case "PyPI":
+		return parsePyPIVersion(str), nil
+	case "Red Hat":
+		return parseRedHatVersion(str), nil
+	case "RubyGems":
+		return parseRubyGemsVersion(str), nil
+	case "Ubuntu":
+		return parseDebianVersion(str), nil
+	}
+
+	return nil, fmt.Errorf("%w %s", ErrUnsupportedEcosystem, ecosystem)
+}
