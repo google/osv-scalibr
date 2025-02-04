5fe4e850383bf1d662b3ea3d235880eeb08bf11d
Gareth Jones <jones258@gmail.com>
2025-02-05 10:26:58 +1300

    refactor: document the returns from `convertToBigInt`

--- semantic/utilities.go
+++ semantic/utilities.go
@@ -5,7 +5,13 @@ import (
 	"math/big"
 )
 
-func convertToBigInt(str string) (*big.Int, error, bool) {
+// convertToBigInt attempts to convert the given str to a big.Int,
+// returning an error if the conversion fails.
+//
+// For convenience, it also returns a boolean indicating whether the given
+// string was a number or not, which can be useful when the string not being
+// a number is actually possible.
+func convertToBigInt(str string) (res *big.Int, err error, isNumber bool) {
 	i, ok := new(big.Int).SetString(str, 10)
 
 	if !ok {
