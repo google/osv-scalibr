c38311e048601749c4f79ad9a942a325bd151159
Erik Varga <erikvarga@google.com>
2025-03-05 04:11:30 -0800

    Add missing package comments.
    PiperOrigin-RevId: 733673903

--- extractor/filesystem/osv/metadata.go
+++ extractor/filesystem/osv/metadata.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package osv defines OSV-specific fields for parsed source packages.
 package osv
 
 // Metadata holds parsing information for packages extracted by an OSV extractor wrapper.
