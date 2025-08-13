58e0b5379f74bc93265ab9dbc41627f74e7216ee
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2025-08-13 14:31:39 +1200

    feat(semantic): support parsing AlmaLinux, Alpaquita, Mageia, openEuler, openSUSE, Rocky Linux, and SUSE ecosystems

--- semantic/parse.go
+++ semantic/parse.go
@@ -43,6 +43,10 @@ func MustParse(str string, ecosystem string) Version {
 func Parse(str string, ecosystem string) (Version, error) {
 	// TODO(#457): support more ecosystems
 	switch ecosystem {
+	case "AlmaLinux":
+		return parseRedHatVersion(str), nil
+	case "Alpaquita":
+		return parseAlpineVersion(str)
 	case "Alpine":
 		return parseAlpineVersion(str)
 	case "Bitnami":
@@ -63,6 +67,8 @@ func Parse(str string, ecosystem string) (Version, error) {
 		return parseSemverVersion(str), nil
 	case "Hex":
 		return parseSemverVersion(str), nil
+	case "Mageia":
+		return parseRedHatVersion(str), nil
 	case "Maven":
 		return parseMavenVersion(str), nil
 	case "MinimOS":
@@ -71,6 +77,10 @@ func Parse(str string, ecosystem string) (Version, error) {
 		return parseSemverVersion(str), nil
 	case "NuGet":
 		return parseNuGetVersion(str), nil
+	case "openEuler":
+		return parseRedHatVersion(str), nil
+	case "openSUSE":
+		return parseRedHatVersion(str), nil
 	case "Packagist":
 		return parsePackagistVersion(str), nil
 	case "Pub":
@@ -79,8 +89,12 @@ func Parse(str string, ecosystem string) (Version, error) {
 		return parsePyPIVersion(str)
 	case "Red Hat":
 		return parseRedHatVersion(str), nil
+	case "Rocky Linux":
+		return parseRedHatVersion(str), nil
 	case "RubyGems":
 		return parseRubyGemsVersion(str), nil
+	case "SUSE":
+		return parseRedHatVersion(str), nil
 	case "SwiftURL":
 		return parseSemverVersion(str), nil
 	case "Ubuntu":
