fe68a8363db02acb159737c382c21291ded4862d
Yuvraj Saxena <ysaxenax@gmail.com>
2025-08-31 16:51:37 +0530

    Add Extractor for Open Virtualization Appliance (.ova) Files #1214
    This PR resolves #1214.
    
    Signed-off-by: Yuvraj Saxena <ysaxenax@gmail.com>

--- purl/purl.go
+++ purl/purl.go
@@ -111,6 +111,8 @@ const (
 	TypeNim = "nim"
 	// TypeLua is pkg:lua purl
 	TypeLua = "lua"
+	// TypeOva is pkg:ova purl
+	TypeOva = "ova"
 )
 
 // PackageURL is the struct representation of the parts that make a package url.
@@ -218,6 +220,7 @@ func validType(t string) bool {
 		TypeAsdf:      true,
 		TypeMacports:  true,
 		TypeWinget:    true,
+		TypeOva:       true,
 	}
 
 	// purl type is case-insensitive, canonical form is lower-case
