fca877066a2eb95f3a2a6f972c898939f0787fd9
alessandro-Doyensec <alessandro.versari@doyensec.com>
2025-09-10 17:25:47 +0200

    init

---
+++ veles/secrets/tinkkeyset/detector.go
@@ -0,0 +1,138 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tinkkeyset
+
+import (
+	"bytes"
+	"encoding/base64"
+	"regexp"
+
+	"github.com/google/osv-scalibr/veles"
+	"github.com/tink-crypto/tink-go/v2/insecurecleartextkeyset"
+	"github.com/tink-crypto/tink-go/v2/keyset"
+)
+
+var (
+
+	// base64Pattern is a generic pattern to detect base64 blobs
+	base64Pattern = regexp.MustCompile(`[A-Za-z0-9+/]{20,}=?=?`)
+
+	// jsonPattern matches correctly tink keyset json strings
+	// thanks to the known `{"primaryKeyId":` start and `]}` ending
+	jsonPattern = regexp.MustCompile(`(?s)\s*\{\s*"primaryKeyId"\s*:\s*\d+,\s*"key"\s*:\s*\[\s*.*?\]\s*\}`)
+
+	// tinkTypeUrl can be found in both binary and json tink keyset encodings
+	tinkTypeUrl = []byte("type.googleapis.com/google.crypto.tink")
+
+	// minBase64Len is an estimate to reduce the number of blobs to decode
+	// note that: len(base64(tinkTypeUrl)) is roughly 50 chars
+	minBase64Len = 60
+)
+
+// Detector is a Veles Detector that finds Tink keysets.
+//
+// It can find Tink keysets in: plain (and escaped) JSON and format, base64 encoded binary and JSON format
+type Detector struct{}
+
+// NewDetector returns a tinkkeyset Detector
+func NewDetector() veles.Detector {
+	return &Detector{}
+}
+
+// Detect implements veles.Detector.
+func (d *Detector) Detect(data []byte) ([]veles.Secret, []int) {
+	res := []veles.Secret{}
+	for _, m := range base64Pattern.FindAllIndex(data, -1) {
+		l, r := m[0], m[1]
+		if (r - l) < minBase64Len {
+			continue
+		}
+
+		// TODO: ideally here I could check for the 3 transposition of `tinkTypeUrl` directly inside
+		// the base64 buf to reduce memory usage
+
+		decoded := make([]byte, base64.StdEncoding.DecodedLen(r-l))
+		n, err := base64.StdEncoding.Decode(decoded, data[l:r])
+		if err != nil || !bytes.Contains(decoded[:n], tinkTypeUrl) {
+			continue
+		}
+		res = append(res, d.find(decoded[:n])...)
+	}
+	if !bytes.Contains(data, tinkTypeUrl) {
+		return res, nil
+	}
+	res = append(res, d.find(data)...)
+	return res, nil
+}
+
+func escape(s []byte) []byte {
+	result := s
+	for range 10 {
+		newResult := bytes.ReplaceAll(result, []byte(`\"`), []byte(`"`))
+		if len(newResult) == len(result) && bytes.Equal(newResult, result) {
+			break
+		}
+		result = newResult
+	}
+	return result
+}
+
+func (d *Detector) find(buf []byte) []veles.Secret {
+	res := d.findJSONKeyset(buf)
+	if len(res) != 0 {
+		return res
+	}
+	return d.findBinaryKeyset(buf)
+}
+
+func (d *Detector) findJSONKeyset(buf []byte) []veles.Secret {
+	res := []veles.Secret{}
+	escaped := escape(buf)
+	for _, m := range jsonPattern.FindAllIndex(escaped, -1) {
+		l, r := m[0], m[1]
+		jsonBuf := escaped[l:r]
+
+		hnd, err := insecurecleartextkeyset.Read(keyset.NewJSONReader(bytes.NewBuffer(jsonBuf)))
+		if err != nil {
+			continue // Not a valid Tink keyset, continue to the next match
+		}
+		// Valid keyset found, convert it back to a canonical JSON string for consistent output.
+		bufOut := new(bytes.Buffer)
+		if err := insecurecleartextkeyset.Write(hnd, keyset.NewJSONWriter(bufOut)); err != nil {
+			return nil
+		}
+		res = append(res, TinkKeySet{Keys: bufOut.String()})
+	}
+	return res
+}
+
+func (d *Detector) findBinaryKeyset(buf []byte) []veles.Secret {
+	hnd, err := insecurecleartextkeyset.Read(keyset.NewBinaryReader(bytes.NewBuffer(buf)))
+	if err != nil {
+		return nil
+	}
+	// Valid binary keyset found, convert it to a JSON string for consistent output.
+	bufOut := new(bytes.Buffer)
+	if err := insecurecleartextkeyset.Write(hnd, keyset.NewJSONWriter(bufOut)); err != nil {
+		return nil
+	}
+	return []veles.Secret{TinkKeySet{Keys: bufOut.String()}}
+}
+
+// MaxSecretLen implements veles.Detector.
+func (d *Detector) MaxSecretLen() uint32 {
+	// TODO: since it's a keyset (multiple keys), I don't know if there is MaxSecretLen
+	return 0
+}
