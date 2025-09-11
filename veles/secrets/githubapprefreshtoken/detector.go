d003c69b0e86a814085fb40ee00db129c2e8efd7
alessandro-Doyensec <alessandro.versari@doyensec.com>
2025-09-11 14:53:32 +0200

    add: boilerplate

---
+++ veles/secrets/githubapprefreshtoken/detector.go
@@ -0,0 +1,37 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package githubapprefreshtoken
+
+import (
+	"regexp"
+
+	"github.com/google/osv-scalibr/veles"
+	"github.com/google/osv-scalibr/veles/secrets/common/simpletoken"
+)
+
+const tokenMaxLen = 300
+
+var tokenPattern = regexp.MustCompile("")
+
+// NewDetector returns a new Veles Detector that finds Github app refresh tokens
+func NewDetector() veles.Detector {
+	return simpletoken.Detector{
+		MaxLen: tokenMaxLen,
+		Re:     tokenPattern,
+		FromMatch: func(b []byte) veles.Secret {
+			return GithubAppRefreshToken{Token: string(b)}
+		},
+	}
+}
