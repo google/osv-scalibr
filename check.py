cce5c61a5cfecda9b7fcc4909df13d1fa2d6ace4
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2025-09-15 11:50:01 +1200

    ci: cleanup workflow

--- check.py
+++
@@ -1,6 +0,0 @@
-with open('veles/secret.go') as f:
-  for line in f.readlines():
-    if line.endswith('\n'):
-      print('line ends with \\n')
-    if line.endswith('\n\r'):
-      print('line ends with \\n\\r')
