c26be0903f551de1ea2c423c7fa0e49c365cc9eb
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2025-09-15 12:13:19 +1200

    feat(os/rpm): allow building on macOS

--- extractor/filesystem/os/rpm/rpm_dummy.go
+++ extractor/filesystem/os/rpm/rpm_dummy.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build !linux
+//go:build windows
 
 // Package rpm extracts packages from rpm database.
 package rpm
