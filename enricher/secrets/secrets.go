f84291f7b9eae6e0ae201b7e2dedcfc2b60146da
alessandro-Doyensec <alessandro.versari@doyensec.com>
2025-09-16 09:46:22 +0200

    add: constructor and conversion logic

--- enricher/secrets/secrets.go
+++ enricher/secrets/secrets.go
@@ -28,6 +28,7 @@ import (
 	"github.com/google/osv-scalibr/veles/secrets/digitaloceanapikey"
 	"github.com/google/osv-scalibr/veles/secrets/dockerhubpat"
 	"github.com/google/osv-scalibr/veles/secrets/gcpsak"
+	githubappservertoservertoken "github.com/google/osv-scalibr/veles/secrets/github/appservertoservertoken"
 	grokxaiapikey "github.com/google/osv-scalibr/veles/secrets/grokxaiapikey"
 	perplexityapikey "github.com/google/osv-scalibr/veles/secrets/perplexityapikey"
 )
@@ -57,6 +58,7 @@ func New() enricher.Enricher {
 		veles.WithValidator(perplexityapikey.NewValidator()),
 		veles.WithValidator(grokxaiapikey.NewAPIValidator()),
 		veles.WithValidator(grokxaiapikey.NewManagementAPIValidator()),
+		veles.WithValidator(githubappservertoservertoken.NewValidator()),
 	)
 	return &Enricher{engine: engine}
 }
--- extractor/filesystem/secrets/secrets.go
+++ extractor/filesystem/secrets/secrets.go
@@ -32,6 +32,7 @@ import (
 	"github.com/google/osv-scalibr/veles/secrets/dockerhubpat"
 	"github.com/google/osv-scalibr/veles/secrets/gcpsak"
 	githubapprefreshtoken "github.com/google/osv-scalibr/veles/secrets/github/apprefreshtoken"
+	githubappservertoservertoken "github.com/google/osv-scalibr/veles/secrets/github/appservertoservertoken"
 	grokxaiapikey "github.com/google/osv-scalibr/veles/secrets/grokxaiapikey"
 	perplexityapikey "github.com/google/osv-scalibr/veles/secrets/perplexityapikey"
 	"github.com/google/osv-scalibr/veles/secrets/privatekey"
@@ -76,6 +77,7 @@ func init() { //nolint:gochecknoinits
 		grokxaiapikey.NewManagementKeyDetector(),
 		privatekey.NewDetector(),
 		githubapprefreshtoken.NewDetector(),
+		githubappservertoservertoken.NewDetector(),
 	})
 	if err != nil {
 		panic(fmt.Sprintf("Unable to initialize default Veles engine: %v", err))
