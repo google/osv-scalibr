31012c15271ab34be0b96c3a0302107fc9d181c5
alessandro-Doyensec <alessandro.versari@doyensec.com>
2025-09-18 12:51:25 +0200

    add: basic detectors

---
+++ veles/secrets/github/personalaccesstoken/finegrained/detector_test.go
@@ -0,0 +1,154 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package finegrained_test
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	"github.com/google/osv-scalibr/veles"
+	"github.com/google/osv-scalibr/veles/secrets/github/apprefreshtoken"
+	"github.com/google/osv-scalibr/veles/secrets/github/personalaccesstoken"
+	"github.com/google/osv-scalibr/veles/secrets/github/personalaccesstoken/finegrained"
+)
+
+// Note: Github tokens are encoded using `` + `` to bypass github security checks
+
+const (
+	testKey        = `github` + `_pat_11ALJFEII0ZiQ19DEeBWSe_apMVlTnpi9UgqDHLAkMLh7iVx63tio9DckV9Rjqas6H4K5W45OQZK6Suog5`
+	anotherTestKey = `github` + `_pat_11ALJFEII0UlnAoY24TCtP_haWQRFX8YZ4vniyajJ3GVbZ5VgNrrEyWFBq3VXgQzQO2M4XQFJMImiHXm6q`
+)
+
+// TestDetector_truePositives tests for cases where we know the Detector
+// will find a Github app refresh tokens.
+func TestDetector_truePositives(t *testing.T) {
+	engine, err := veles.NewDetectionEngine([]veles.Detector{finegrained.NewDetector()})
+	if err != nil {
+		t.Fatal(err)
+	}
+	cases := []struct {
+		name  string
+		input string
+		want  []veles.Secret
+	}{{
+		name:  "simple matching string",
+		input: testKey,
+		want: []veles.Secret{
+			personalaccesstoken.GithubPersonalAccessToken{Token: testKey},
+		},
+	}, {
+		name:  "simple matching string another key",
+		input: anotherTestKey,
+		want: []veles.Secret{
+			personalaccesstoken.GithubPersonalAccessToken{Token: anotherTestKey},
+		},
+	}, {
+		name:  "match at end of string",
+		input: `API_TOKEN=` + testKey,
+		want: []veles.Secret{
+			personalaccesstoken.GithubPersonalAccessToken{Token: testKey},
+		},
+	}, {
+		name:  "match in middle of string",
+		input: `API_TOKEN="` + testKey + `"`,
+		want: []veles.Secret{
+			personalaccesstoken.GithubPersonalAccessToken{Token: testKey},
+		},
+	}, {
+		name:  "multiple matches",
+		input: testKey + testKey + testKey,
+		want: []veles.Secret{
+			personalaccesstoken.GithubPersonalAccessToken{Token: testKey},
+			personalaccesstoken.GithubPersonalAccessToken{Token: testKey},
+			personalaccesstoken.GithubPersonalAccessToken{Token: testKey},
+		},
+	}, {
+		name:  "multiple distinct matches",
+		input: testKey + "\n" + anotherTestKey,
+		want: []veles.Secret{
+			personalaccesstoken.GithubPersonalAccessToken{Token: testKey},
+			personalaccesstoken.GithubPersonalAccessToken{Token: anotherTestKey},
+		},
+	}, {
+		name: "larger input containing key",
+		input: fmt.Sprintf(`
+:test_api_key: do-test
+:API_TOKEN: %s
+		`, testKey),
+		want: []veles.Secret{
+			personalaccesstoken.GithubPersonalAccessToken{Token: testKey},
+		},
+	}, {
+		name:  "potential match longer than max key length",
+		input: testKey + `extra`,
+		want: []veles.Secret{
+			personalaccesstoken.GithubPersonalAccessToken{Token: testKey},
+		},
+	}}
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			got, err := engine.Detect(t.Context(), strings.NewReader(tc.input))
+			if err != nil {
+				t.Errorf("Detect() error: %v, want nil", err)
+			}
+			if diff := cmp.Diff(tc.want, got, cmpopts.EquateEmpty()); diff != "" {
+				t.Errorf("Detect() diff (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+// TestDetector_trueNegatives tests for cases where we know the Detector
+// will not find a Github app refresh tokens.
+func TestDetector_trueNegatives(t *testing.T) {
+	engine, err := veles.NewDetectionEngine([]veles.Detector{apprefreshtoken.NewDetector()})
+	if err != nil {
+		t.Fatal(err)
+	}
+	cases := []struct {
+		name  string
+		input string
+		want  []veles.Secret
+	}{{
+		name:  "empty input",
+		input: "",
+	}, {
+		name:  "short key should not match",
+		input: testKey[:len(testKey)-1],
+	}, {
+		name:  "invalid character in key should not match",
+		input: `github` + `_pat_11ALJFEII0Zi+19DEeBWSe_apMVlTnpi9UgqDHLAkMLh7iVx63tio9DckV9Rjqas6H4K5W45OQZK6Suog5`,
+	}, {
+		name:  "incorrect prefix should not match",
+		input: `Eop_v1_OWOCPzqKuy3J4w53QpkLfffjBUJSh5yLnFHj7wiyR0NDadVOcykNkoqhoYYXM1yy2sOpAu0lG8fw`,
+	}, {
+		name:  "prefix missing dash should not match",
+		input: `github` + `pat11ALJFEII0ZiQ19DEeBWSe_apMVlTnpi9UgqDHLAkMLh7iVx63tio9DckV9Rjqas6H4K5W45OQZK6Suog5`,
+	}}
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			got, err := engine.Detect(t.Context(), strings.NewReader(tc.input))
+			if err != nil {
+				t.Errorf("Detect() error: %v, want nil", err)
+			}
+			if diff := cmp.Diff(tc.want, got, cmpopts.EquateEmpty()); diff != "" {
+				t.Errorf("Detect() diff (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
