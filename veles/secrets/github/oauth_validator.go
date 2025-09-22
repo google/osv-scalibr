02337be8e1248f4889c3e15c367782ae8f189a93
alessandro-Doyensec <alessandro.versari@doyensec.com>
2025-09-22 19:04:58 +0200

    edit: move common validation logic into an helper pkg

--- veles/secrets/github/oauth_validator.go
+++ veles/secrets/github/oauth_validator.go
@@ -16,11 +16,10 @@ package github
 
 import (
 	"context"
-	"fmt"
-	"io"
 	"net/http"
 
 	"github.com/google/osv-scalibr/veles"
+	"github.com/google/osv-scalibr/veles/secrets/github/validate"
 )
 
 // OAuthTokenValidator validates Github OAuth token via the Github API endpoint.
@@ -53,32 +52,5 @@ func NewOAuthTokenValidator(opts ...OAuthTokenValidatorOption) *OAuthTokenValida
 
 // Validate checks whether the given Github OAuth token is valid.
 func (v *OAuthTokenValidator) Validate(ctx context.Context, key OAuthToken) (veles.ValidationStatus, error) {
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet,
-		"https://api.github.com/user", nil)
-	if err != nil {
-		return veles.ValidationFailed, fmt.Errorf("unable to create HTTP request: %w", err)
-	}
-	req.Header.Set("Authorization", "Bearer "+key.Token)
-	req.Header.Set("Accept", "application/vnd.github+json")
-	//nolint:canonicalheader // This header is set as "X-GitHub-Api-Version" exactly as documented by GitHub.
-	req.Header.Set("X-GitHub-Api-Version", "2022-11-28")
-
-	res, err := v.httpC.Do(req)
-	if err != nil {
-		return veles.ValidationFailed, fmt.Errorf("HTTP GET failed: %w", err)
-	}
-	defer res.Body.Close()
-	_, err = io.ReadAll(res.Body)
-	if err != nil {
-		return veles.ValidationFailed, fmt.Errorf("failed to read response body: %w", err)
-	}
-
-	switch res.StatusCode {
-	case http.StatusOK, http.StatusForbidden:
-		return veles.ValidationValid, nil
-	case http.StatusUnauthorized:
-		return veles.ValidationInvalid, nil
-	default:
-		return veles.ValidationFailed, nil
-	}
+	return validate.Validate(ctx, v.httpC, "/user", key.Token)
 }
