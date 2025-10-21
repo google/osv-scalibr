97150f74e5672165e1a5c7aeedb64a7a8c25ac4e
alessandro-Doyensec <alessandro.versari@doyensec.com>
2025-10-21 19:36:11 +0200

    fix: lint add: more precise interface research

--- linter/plugger/main.go
+++ linter/plugger/main.go
@@ -1,3 +1,18 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package main contains the plugger entrypoint
 package main
 
 import (
@@ -15,13 +30,14 @@ var (
 	fExcludePkgPattern string
 )
 
-func init() {
+func setFlags() {
 	flag.StringVar(&fIPattern, "interface", "", `regex pattern for plugin interfaces, ex: 'github\.com/package.MyInterface|.*\.OtherInterface'`)
 	// setting a^ as default to include everything
 	flag.StringVar(&fExcludePkgPattern, "exclude-pkg", "a^", `regex pattern for pkg to exclude, ex: 'github\.com/package/testing/.*'`)
 }
 
 func main() {
+	setFlags()
 	flag.Parse()
 
 	pkgs := flag.Args()
--- linter/plugger/plugger/implementations.go
+++ linter/plugger/plugger/implementations.go
@@ -1,3 +1,17 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package plugger
 
 import (
@@ -16,7 +30,6 @@ func FindImplementations(pkgs []*packages.Package, interfaces []*types.Named) ma
 	filter := []ast.Node{(*ast.TypeSpec)(nil)}
 
 	for _, pkg := range pkgs {
-
 		inspector.New(pkg.Syntax).Preorder(filter, func(n ast.Node) {
 			typeSpec := n.(*ast.TypeSpec)
 
@@ -55,10 +68,10 @@ func doesImplement(named, iface *types.Named) bool {
 	if iface.TypeParams().Len() > 0 {
 		// Collect type arguments by trying to infer from methods
 		typeArgs := make([]types.Type, iface.TypeParams().Len())
-		for i := 0; i < iface.TypeParams().Len(); i++ {
+		for i := range iface.TypeParams().Len() {
 			// For simplicity, try to infer from the first method with enough parameters
 			inferred := false
-			for j := 0; j < named.NumMethods(); j++ {
+			for j := range named.NumMethods() {
 				m := named.Method(j)
 				sig, ok := m.Type().(*types.Signature)
 				if !ok || sig.Params().Len() <= i {
