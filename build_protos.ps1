7a8d5139d6ab8853ada0b7d6489f551a4ef06867
edwing <84491653+roblesyq@users.noreply.github.com>
2025-11-09 06:25:12 -0600

    Update build_protos.ps1

--- build_protos.ps1
+++ build_protos.ps1
@@ -1,4 +1,1 @@
-Remove-Item -Recurse -Force binary/proto/*_go_proto
-protoc -I=binary --go_out=binary/proto binary/proto/*.proto
-Move-Item binary/proto/github.com/google/scalibr/binary/proto/* binary/proto/
-Remove-Item -Recurse -Force binary/proto/github.com
+
