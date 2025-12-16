b6bad692d24ae90e99aa06ec6e5aa2cf970c2708
Joey Lau <joeylauy@google.com>
2025-12-16 04:22:25 +0000

    Clean up test data

--- enricher/reachability/rust/testdata/real-rust-project/src/main.rs
+++ enricher/reachability/rust/testdata/real-rust-project/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     let mut v: SmallVec<[i32; 4]> = SmallVec::new();
     v.push(1);
 
-    // This is an unreachable vulnerable function
+    // This is a vulnerable function (RUSTSEC-2021-0003)
     // v.insert_many(1, [2, 3]);
 
     println!("{:?}", v);
--- enricher/reachability/rust/testdata/rust-project/src/main.rs
+++
@@ -1,6 +0,0 @@
-use time::OffsetDateTime;
-
-fn main() {
-    // let now = OffsetDateTime::now_local();
-    println!("Hello, World. The current time is {}", OffsetDateTime::now_utc());
-}
