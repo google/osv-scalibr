85328c86b9c957ab767e3ed4b8fadddb089b8914
Maciej Trzos <mtrzos@google.com>
2026-01-22 23:50:45 -0800

    Migrate the runtime extractors to use the PluginConfig.
    PiperOrigin-RevId: 859958256

--- extractor/filesystem/runtime/nodejs/nvm/nvm.go
+++ extractor/filesystem/runtime/nodejs/nvm/nvm.go
@@ -29,6 +29,8 @@ import (
 	"github.com/google/osv-scalibr/inventory"
 	"github.com/google/osv-scalibr/plugin"
 	"github.com/google/osv-scalibr/purl"
+
+	cpb "github.com/google/osv-scalibr/binary/proto/config_go_proto"
 )
 
 const (
@@ -40,7 +42,7 @@ const (
 type Extractor struct{}
 
 // New returns a new instance of the extractor.
-func New() filesystem.Extractor { return &Extractor{} }
+func New(cfg *cpb.PluginConfig) (filesystem.Extractor, error) { return &Extractor{}, nil }
 
 // Name of the extractor.
 func (e Extractor) Name() string { return Name }
