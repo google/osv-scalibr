2d4857d6f488bc1020e9b0268b40259c5b1d72af
Sanjay Jangid <136222049+sanjay20m@users.noreply.github.com>
2026-02-10 11:29:42 +0530

    Update discordbottoken.go

--- veles/secrets/discordbottoken/discordbottoken.go
+++ veles/secrets/discordbottoken/discordbottoken.go
@@ -1,18 +1,21 @@
+/ Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package discordbottoken contains a Veles Secret type and a Detector for Discord bot tokens.
 package discordbottoken
-
-import "github.com/google/osv-scalibr/veles/secrets"
-
-const (
-	// Type is the unique secret type identifier
-	Type secrets.Type = "discord_bot_token"
-)
 
 // DiscordBotToken represents a detected Discord bot token.
 type DiscordBotToken struct {
-	secrets.SecretBase
-}
-
-// Type returns the secret type.
-func (s *DiscordBotToken) Type() secrets.Type {
-	return Type
+	Token string
 }
