f3dd6655d2e596b4b23f4bee9a24724c1df13d5f
mzfr <github@mzfr.in>
2026-02-26 23:24:20 +0800

    Fixed lint issue in containerd (untouched file in this detector)

--- extractor/filesystem/containers/containerd/metadata.go
+++ extractor/filesystem/containers/containerd/metadata.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package containerd extracts container inventory from containerd metadata.
 package containerd
 
 import (
