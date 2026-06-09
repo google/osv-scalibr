1ef13be1167980a2916ff1995ec430dcc5fe4f0b
Szymon Drosdzol <szymon@doyensec.com>
2026-06-09 16:56:32 +0200

    Cleanup unused model

--- veles/sensitiveinformation/ssn/modelssn.go
+++
@@ -1,8 +0,0 @@
-package ssn
-
-import "github.com/google/osv-scalibr/veles/sensitiveinformation"
-
-type ModelSocialSecurityNumber struct {
-	sensitiveinformation.SensitiveInformation
-	Number string
-}
