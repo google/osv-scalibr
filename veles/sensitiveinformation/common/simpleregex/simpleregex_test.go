8ecdd91ec29e2a027547bf0155a0a0f853d266c3
Szymon Drosdzol <szymon@doyensec.com>
2026-06-10 15:22:57 +0200

    CherryPick SimpleRegex changes 2
    FromMatch will return SensitiveInformation after all
    
    Prevent SimpleRegex from immediately rejecting findings when KeywordsRe
    doesn't match
    
    This allows adjusting Confidence inside FromMatch callback based on
    whether the the KeywordsRe match the surrounding of the finding itself.
    
    Add parameter names for FromMatch callback for clarity
    
    SimpleRegex tests for the keywords checking behavior
    
    fix lint

--- veles/sensitiveinformation/common/simpleregex/simpleregex_test.go
+++ veles/sensitiveinformation/common/simpleregex/simpleregex_test.go
@@ -43,7 +43,8 @@ func TestDetect_truePositives(t *testing.T) {
 		in        []byte
 		want      []veles.Secret
 		wantPos   []int
-		fromMatch func([]byte) (sensitiveinformation.SensitiveInformation, bool)
+		wantFlags []bool
+		fromMatch func([]byte, bool) (sensitiveinformation.SensitiveInformation, bool)
 	}{
 		{
 			name:   "match only",
@@ -123,7 +124,7 @@ BAZ
 				fakeSensitiveInformation([]byte("BAR")),
 			},
 			wantPos: []int{4},
-			fromMatch: func(b []byte) (sensitiveinformation.SensitiveInformation, bool) {
+			fromMatch: func(b []byte, contextMatch bool) (sensitiveinformation.SensitiveInformation, bool) {
 				if string(b) == "FOO" {
 					return sensitiveinformation.SensitiveInformation{}, false
 				}
@@ -149,9 +150,14 @@ BAZ
 			before:   4,
 			keywords: []string{"abc", "def"},
 			want: []veles.Secret{
+				fakeSensitiveInformation([]byte("FOO")),
 				fakeSensitiveInformation([]byte("BAR")),
 			},
-			wantPos: []int{8},
+			wantPos: []int{0, 8},
+			wantFlags: []bool{
+				false,
+				true,
+			},
 		}, {
 			name:     "keywords after",
 			regexp:   "[A-Z]{3}",
@@ -161,8 +167,15 @@ BAZ
 			keywords: []string{"abc", "def"},
 			want: []veles.Secret{
 				fakeSensitiveInformation([]byte("FOO")),
+				fakeSensitiveInformation([]byte("DEF")),
+				fakeSensitiveInformation([]byte("BAR")),
 			},
-			wantPos: []int{0},
+			wantPos: []int{0, 4, 8},
+			wantFlags: []bool{
+				true,
+				false,
+				false,
+			},
 		}, {
 			name:     "keywords before and after",
 			regexp:   "[A-Z]{3}",
@@ -172,10 +185,20 @@ BAZ
 			after:    4,
 			keywords: []string{"abc", "def"},
 			want: []veles.Secret{
+				fakeSensitiveInformation([]byte("ABC")),
 				fakeSensitiveInformation([]byte("FOO")),
+				fakeSensitiveInformation([]byte("DEF")),
 				fakeSensitiveInformation([]byte("BAR")),
+				fakeSensitiveInformation([]byte("ABC")),
 			},
-			wantPos: []int{4, 12},
+			wantPos: []int{0, 4, 8, 12, 16},
+			wantFlags: []bool{
+				false,
+				true,
+				false,
+				true,
+				false,
+			},
 		}, {
 			name:     "keywords matches regex",
 			regexp:   "[A-Z]{3}",
@@ -184,26 +207,37 @@ BAZ
 			before:   4,
 			keywords: []string{"abc", "def"},
 			want: []veles.Secret{
+				fakeSensitiveInformation([]byte("ABC")),
 				fakeSensitiveInformation([]byte("DEF")),
 				fakeSensitiveInformation([]byte("ABC")),
 			},
-			wantPos: []int{4, 8},
+			wantPos: []int{0, 4, 8},
+			wantFlags: []bool{
+				false,
+				true,
+				true,
+			},
 		},
 	}
 	for _, tc := range cases {
 		t.Run(tc.name, func(t *testing.T) {
-			if tc.fromMatch == nil {
-				tc.fromMatch = func(b []byte) (sensitiveinformation.SensitiveInformation, bool) {
+			fromMatch := tc.fromMatch
+			if fromMatch == nil {
+				fromMatch = func(b []byte, _ bool) (sensitiveinformation.SensitiveInformation, bool) {
 					return fakeSensitiveInformation(b), true
 				}
 			}
+			var gotFlags []bool
 			d := Detector{
 				MaxLen:              tc.maxLen,
 				Re:                  regexp.MustCompile(tc.regexp),
 				ContextWindowBefore: tc.before,
 				ContextWindowAfter:  tc.after,
 				KeywordsRe:          KeywordsRe(tc.keywords),
-				FromMatch:           tc.fromMatch,
+				FromMatch: func(b []byte, contextMatch bool) (sensitiveinformation.SensitiveInformation, bool) {
+					gotFlags = append(gotFlags, contextMatch)
+					return fromMatch(b, contextMatch)
+				},
 			}
 			got, gotPos := d.Detect(tc.in)
 			if diff := cmp.Diff(tc.want, got, cmpopts.EquateEmpty()); diff != "" {
@@ -211,6 +245,73 @@ BAZ
 			}
 			if diff := cmp.Diff(tc.wantPos, gotPos, cmpopts.EquateEmpty()); diff != "" {
 				t.Errorf("Detect() diff (-want +got):\n%s", diff)
+			}
+			wantFlags := tc.wantFlags
+			if wantFlags == nil {
+				wantFlags = make([]bool, len(gotFlags))
+			}
+			if diff := cmp.Diff(wantFlags, gotFlags, cmpopts.EquateEmpty()); diff != "" {
+				t.Errorf("FromMatch() keywordMatch diff (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestDetect_passesKeywordContextMatchToFromMatch(t *testing.T) {
+	type callbackCall struct {
+		Match        string
+		ContextMatch bool
+	}
+
+	cases := []struct {
+		name     string
+		keywords []string
+		in       []byte
+		before   uint32
+		after    uint32
+		want     []callbackCall
+	}{
+		{
+			name:     "keyword_match_and_no_keyword_match",
+			keywords: []string{"abc", "def"},
+			in:       []byte("abc FOO x BAR yyy BAZ def"),
+			before:   4,
+			after:    4,
+			want: []callbackCall{
+				{Match: "FOO", ContextMatch: true},
+				{Match: "BAR", ContextMatch: false},
+				{Match: "BAZ", ContextMatch: true},
+			},
+		},
+		{
+			name:     "no_keywords_regexp",
+			keywords: nil,
+			in:       []byte("abc FOO def"),
+			before:   4,
+			after:    4,
+			want: []callbackCall{
+				{Match: "FOO", ContextMatch: false},
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			var got []callbackCall
+			d := Detector{
+				MaxLen:              3,
+				Re:                  regexp.MustCompile("[A-Z]{3}"),
+				ContextWindowBefore: tc.before,
+				ContextWindowAfter:  tc.after,
+				KeywordsRe:          KeywordsRe(tc.keywords),
+				FromMatch: func(b []byte, contextMatch bool) (sensitiveinformation.SensitiveInformation, bool) {
+					got = append(got, callbackCall{Match: string(b), ContextMatch: contextMatch})
+					return fakeSensitiveInformation(b), true
+				},
+			}
+			d.Detect(tc.in)
+			if diff := cmp.Diff(tc.want, got); diff != "" {
+				t.Errorf("FromMatch calls diff (-want +got):\n%s", diff)
 			}
 		})
 	}
@@ -234,26 +335,10 @@ func TestDetect_trueNegatives(t *testing.T) {
 		regexp: "FOO",
 		maxLen: 3,
 		in:     []byte("BAR"),
-	}, {
-		name:     "no keyword match",
-		regexp:   "FOO",
-		keywords: []string{"abc", "def"},
-		before:   4,
-		after:    4,
-		maxLen:   3,
-		in:       []byte("FOO"),
-	}, {
-		name:     "keyword ahead of context window",
-		regexp:   "FOO",
-		keywords: []string{"abc", "def"},
-		before:   3,
-		after:    3,
-		maxLen:   3,
-		in:       []byte("abc FOO def"),
 	}}
 	for _, tc := range cases {
 		t.Run(tc.name, func(t *testing.T) {
-			fromMatch := func(b []byte) (sensitiveinformation.SensitiveInformation, bool) {
+			fromMatch := func(b []byte, contextMatch bool) (sensitiveinformation.SensitiveInformation, bool) {
 				return fakeSensitiveInformation(b), true
 			}
 			d := Detector{
---
+++ veles/sensitiveinformation/ssn/detector.go
@@ -0,0 +1,87 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package ssn implements SSN detection logic.
+package ssn
+
+import (
+	"bytes"
+	"regexp"
+
+	"github.com/google/osv-scalibr/veles"
+	"github.com/google/osv-scalibr/veles/sensitiveinformation"
+	"github.com/google/osv-scalibr/veles/sensitiveinformation/common/simpleregex"
+)
+
+const maxSecretLength = 11
+
+// https://www.protecto.ai/blog/personal-dataset-sample-u-s-social-security-number-ssn-download-pii-data-examples-2/
+// https://www.ssa.gov/history/ssn/geocard.html
+var ssnRe = regexp.MustCompile(`\b[0-8]\d{2}-\d{2}-\d{4}\b`)
+
+var commonExamples = map[string]struct{}{
+	"123-45-6789": {},
+
+	"111-11-1111": {},
+	"222-22-2222": {},
+	"333-33-3333": {},
+	"444-44-4444": {},
+	"555-55-5555": {},
+	"777-77-7777": {},
+	"888-88-8888": {},
+	"999-99-9999": {},
+
+	// https://www.ssa.gov/history/ssn/misused.html
+	"078-05-1120": {},
+}
+
+// NewDetector returns a Detector, that finds US Social Security Numbers (SSNs)
+func NewDetector() veles.Detector {
+	return simpleregex.Detector{
+		MaxLen: maxSecretLength,
+		Re:     ssnRe,
+		FromMatch: func(b []byte, contextMatch bool) (sensitiveinformation.SensitiveInformation, bool) {
+			ssn := string(b)
+			if !validSSN(ssn) {
+				return sensitiveinformation.SensitiveInformation{}, false
+			}
+
+			finding := sensitiveinformation.SensitiveInformation{
+				InfoType: sensitiveinformation.InfoType{
+					Name:        "SOCIAL_SECURITY_NUMBER",
+					Sensitivity: sensitiveinformation.SensitivityLevelHigh,
+				},
+				Likelihood: sensitiveinformation.LikelihoodLikely,
+				Raw:        bytes.Clone(b),
+			}
+
+			return finding, true
+		},
+	}
+}
+
+func validSSN(s string) bool {
+	if _, ok := commonExamples[s]; ok {
+		return false
+	}
+
+	area := s[0:3]
+	group := s[4:6]
+	serial := s[7:11]
+
+	return area != "000" &&
+		area != "666" &&
+		group != "00" &&
+		serial != "0000"
+}
