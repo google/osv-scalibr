73ae381768ba042bbc1794b82e9923c735cf5c05
Szymon Drosdzol <szymon@doyensec.com>
2026-06-22 19:17:44 +0200

    Update and add source for IBAN formats

--- veles/sensitiveinformation/iban/detector.go
+++ veles/sensitiveinformation/iban/detector.go
@@ -29,28 +29,31 @@ const maxSecretLength = 41
 
 var ibanRe = regexp.MustCompile(`(?i)\b[A-Z]{2}\d{2}(?:[A-Z0-9]{11,30}|(?: [A-Z0-9]{4}){2,7}(?: [A-Z0-9]{1,3})?)\b`)
 
+// https://en.wikipedia.org/wiki/International_Bank_Account_Number#IBAN_formats_by_country
 var countryLengths = map[string]int{
 	"AD": 24, "AE": 23, "AL": 28, "AT": 20, "AZ": 28,
-	"BA": 20, "BE": 16, "BG": 22, "BH": 22, "BI": 16, "BR": 29, "BY": 28,
+	"BA": 20, "BE": 16, "BG": 22, "BH": 22, "BI": 27, "BR": 29, "BY": 28,
 	"CH": 21, "CR": 22, "CY": 28, "CZ": 24,
 	"DE": 22, "DJ": 27, "DK": 18, "DO": 28,
 	"EE": 20, "EG": 29, "ES": 24,
-	"FI": 18, "FO": 18, "FR": 27,
+	"FI": 18, "FK": 18, "FO": 18, "FR": 27,
 	"GB": 22, "GE": 22, "GI": 23, "GL": 18, "GR": 27, "GT": 28,
-	"HR": 21, "HU": 28,
+	"HN": 28, "HR": 21, "HU": 28,
 	"IE": 22, "IL": 23, "IQ": 23, "IS": 26, "IT": 27,
 	"JO": 30,
 	"KW": 30, "KZ": 20,
 	"LB": 28, "LC": 32, "LI": 21, "LT": 20, "LU": 20, "LV": 21, "LY": 25,
-	"MC": 27, "MD": 24, "ME": 22, "MK": 19, "MR": 27, "MT": 31, "MU": 30,
-	"NL": 18, "NO": 15,
+	"MC": 27, "MD": 24, "ME": 22, "MK": 19, "MN": 20, "MR": 27, "MT": 31, "MU": 30,
+	"NI": 28, "NL": 18, "NO": 15,
+	"OM": 23,
 	"PK": 24, "PL": 28, "PS": 29, "PT": 25,
 	"QA": 29,
 	"RO": 24, "RS": 22, "RU": 33,
-	"SA": 24, "SC": 31, "SD": 18, "SE": 24, "SI": 19, "SK": 24, "SM": 27, "ST": 25, "SV": 28,
+	"SA": 24, "SC": 31, "SD": 18, "SE": 24, "SI": 19, "SK": 24, "SM": 27, "SO": 23, "ST": 25, "SV": 28,
 	"TL": 23, "TN": 24, "TR": 26,
 	"UA": 29,
 	"VA": 22, "VG": 24,
+	"YE": 30,
 	"XK": 20,
 }
 
