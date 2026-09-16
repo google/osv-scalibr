12149f83da35bdb84383b2b4988521ccad4e1fa3
Tejas Pravinbhai Patel <31176477+tejas5038@users.noreply.github.com>
2026-09-16 02:24:57 -0500

    fix: preserve 32-bit statfs conversion without lint failure

--- extractor/filesystem/embeddedfs/common/common_linux.go
+++ extractor/filesystem/embeddedfs/common/common_linux.go
@@ -24,5 +24,5 @@ func freeSpaceBytes(dir string) int64 {
 	if err := syscall.Statfs(dir, &stat); err != nil {
 		return 0
 	}
-	return int64(stat.Bavail) * int64(stat.Bsize)
+	return int64(stat.Bavail) * int64(stat.Bsize) //nolint:unconvert // Bsize is 32-bit on some Linux architectures.
 }
