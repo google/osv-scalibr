faf9acd8728ea3a4d325d200ff9d8a8f8dcc7730
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2026-09-18 16:01:15 +1200

    feat(os/rpm): build for Windows

--- extractor/filesystem/os/rpm/rpm.go
+++ extractor/filesystem/os/rpm/rpm.go
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build !windows
-
 // Package rpm extracts packages from rpm database.
 package rpm
 
