aa8412c5aec41fe1e802cdbfade34ed3102abb14
Robert McConnell <robert@mcc0nnell.org>
2026-09-18 02:26:38 +0000

    fix: cover git+http commit parameters
    Signed-off-by: Robert McConnell <robert@mcc0nnell.org>

--- extractor/filesystem/language/javascript/internal/commitextractor/commit_test.go
+++ extractor/filesystem/language/javascript/internal/commitextractor/commit_test.go
@@ -57,6 +57,11 @@ func TestTryExtractCommit(t *testing.T) {
 			wantCommit: "3ed06e5097ab929f69755676fee419318aaec73a",
 		},
 		{
+			name:       "git+http commit parameter",
+			resolution: "git+http://example.com/my-org/my-package.git#commit=3ed06e5097ab929f69755676fee419318aaec73a",
+			wantCommit: "3ed06e5097ab929f69755676fee419318aaec73a",
+		},
+		{
 			name:       "npm registry tarball commit fragment",
 			resolution: "https://registry.npmjs.org/lodash/-/lodash-4.17.20.tgz#commit=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
 			wantCommit: "",
